31c8d8fad8a43cc3dfd21a9a433bd911e0e7a065
Douglas Braga <douglas.braga@freterapido.com>
2022-06-25 23:09:42 -0300

    config: initialize config firebase

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDHKgdoQBgP-xjJMTUJIpe4hVdUr1UVw1M",
+    authDomain: "miniblog-65961.firebaseapp.com",
+    projectId: "miniblog-65961",
+    storageBucket: "miniblog-65961.appspot.com",
+    messagingSenderId: "767885796100",
+    appId: "1:767885796100:web:260b5f87add069014a6651"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
